0890d12299860b2a9113c3061cc91152b84e5a5e
zherdev <zherdevivanp@gmail.com>
2019-03-25 02:20:00 +0300

    add opera support

--- src/test/java/BrowsersFactory.java
+++ src/test/java/BrowsersFactory.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
 
 public enum BrowsersFactory {
     chrome {
@@ -16,7 +18,7 @@ public enum BrowsersFactory {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--disable-notifications");
 
-            return  new ChromeDriver(options);
+            return new ChromeDriver(options);
         }
     },
 
@@ -29,6 +31,17 @@ public enum BrowsersFactory {
             options.addPreference("dom.webnotifications.enabled", false);
 
             return new FirefoxDriver(options);
+        }
+    },
+
+    opera {
+        public WebDriver create() {
+            updateProperty("opera");
+            OperaOptions options = new OperaOptions();
+            options.setBinary(System.getProperty("opera-binary"));
+            options.addArguments("--disable-notifications");
+
+            return  new OperaDriver(options);
         }
     };
 
