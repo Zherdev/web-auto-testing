f48ff869c162cc276369acce5d765cd4cf7f7c72
zherdev <zherdevivanp@gmail.com>
2019-03-24 23:55:00 +0300

    add BrowsersFactory

---
+++ src/test/java/BrowsersFactory.java
@@ -0,0 +1,40 @@
+/*
+ BrowsersFactory
+
+ Ivan Zherdev, 2019
+ */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public enum BrowsersFactory {
+    chrome {
+        public WebDriver create() {
+            updateProperty("chrome");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-notifications");
+
+            return  new ChromeDriver(options);)
+        }
+    },
+
+    firefox {
+        public WebDriver create() {
+            updateProperty("firefox");
+            System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+            System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "./src/test/java/firefox_logs/log");
+            FirefoxOptions options = new FirefoxOptions();
+            options.addPreference("dom.webnotifications.enabled", false);
+
+            return new FirefoxDriver(options);
+        }
+    };
+
+    void updateProperty(String browserName) {
+        System.out.println("Запускаем браузер " + browserName + "...");
+        if (System.getProperty("browser") == null) System.setProperty("browser", browserName);
+    }
+
+}
